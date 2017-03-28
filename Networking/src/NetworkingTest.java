5b112cf189ba22be669c192fb90561b8643ebb44
Joris <joris.oosterhuis007@gmail.com>
2017-03-28 12:34:56 +0200

    Created network module

---
+++ Networking/src/NetworkingTest.java
@@ -0,0 +1,6 @@
+public class NetworkingTest {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Networking/src/command/Communicator.java
@@ -0,0 +1,19 @@
+package command;
+
+public class Communicator {
+
+    private String host;
+    private int port;
+
+    public Communicator(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void login(){
+        
+    }
+
+
+
+}
