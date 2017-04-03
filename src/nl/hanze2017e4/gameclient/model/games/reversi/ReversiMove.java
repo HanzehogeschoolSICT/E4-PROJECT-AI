49725fb1b6e546d7a23068f2795bbdde56a342d6
Joris Oosterhuis <joris.oosterhuis007@gmail.com>
2017-04-03 19:03:24 +0200

    Added methods to reversimove

--- src/nl/hanze2017e4/gameclient/model/games/reversi/ReversiMove.java
+++ src/nl/hanze2017e4/gameclient/model/games/reversi/ReversiMove.java
@@ -11,21 +11,49 @@ public class ReversiMove {
     private int move;
     private Board moveBoard;
     private int score;
-    private ArrayList<ReversiMove> nextMoves;
+    private ArrayList<ReversiMove> nextMovesList;
 
-    public ReversiMove(Player playerMoves, int move) {
+    public ReversiMove(Player playerMoves, int move, Board board) {
         this.playerMoves = playerMoves;
         this.move = move;
-        this.moveBoard = makeBoardAfterMove(move);
-        this.nextMoves = new ArrayList<>();
+        this.moveBoard = makeBoardAfterMove(move, board);
+        this.nextMovesList = new ArrayList<>();
     }
 
-    private Board makeBoardAfterMove(int move) {
-        //TODO make board after move --JORIS
-        return null;
+    private Board makeBoardAfterMove(int move, Board board) {
+        Board boardAfterMove = new Board(board);
+        boardAfterMove.setPlayerAtPos(playerMoves, move);
+        return boardAfterMove;
     }
+
+
+
 
     public int getMove() {
         return move;
+    }
+
+    public ArrayList<ReversiMove> getNextMovesList(){
+        return nextMovesList;
+    }
+
+    public void addMoveToNextMoves(ReversiMove nextMove){
+        nextMovesList.add(nextMove);
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Board getMoveBoard() {
+        return moveBoard;
+    }
+
+    public void setMoveBoard(Board moveBoard) {
+        this.moveBoard = moveBoard;
     }
 }
