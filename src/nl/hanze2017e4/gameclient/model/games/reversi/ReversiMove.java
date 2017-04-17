e601fdd00a06f134d5de8859e9866754d34ba237
Joris Oosterhuis <joris.oosterhuis007@gmail.com>
2017-04-17 21:25:57 +0200

    Possible fix for adding scores

--- src/nl/hanze2017e4/gameclient/model/games/reversi/ReversiMove.java
+++ src/nl/hanze2017e4/gameclient/model/games/reversi/ReversiMove.java
@@ -28,13 +28,15 @@ public class ReversiMove {
         ReversiMove bestReversiMove = reversiAI.getBestMove();
 
         //To handle a pass when no move can be made by the opponent.
-        if (!(bestReversiMove.getMove() == -1) && !(playerMoves.equals(boardAfterMove.getPlayerTwo()))) {
-            if (playerMoves.equals(boardAfterMove.getPlayerOne())) {
-                score += bestReversiMove.getScore();
-                priority = bestReversiMove.getPriority();
-            } else {
+        if (playerMoves.equals(boardAfterMove.getPlayerOne())) {
+            //us
+            score += bestReversiMove.getScore();
+            priority = bestReversiMove.getPriority();
+        } else {
+            //opponent
+            if (!(bestReversiMove.getMove() == -1)) {
                 score -= bestReversiMove.getScore();
-                priority = bestReversiMove.getPriority() * -1;
+                priority = (bestReversiMove.getPriority() * -1);
             }
         }
     }
